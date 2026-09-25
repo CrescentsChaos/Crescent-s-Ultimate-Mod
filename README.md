b1dd49bed8d0d5996b4abcbffed83208f2364e05
Crescent <trtonoy2@gmail.com>
2026-09-26 00:31:31 +0600

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Crescent&#39;s Ultimate Mod
+
+## Setup
+
+For setup instructions, please see the [Fabric Documentation page](https://docs.fabricmc.net/develop/getting-started/creating-a-project#setting-up) related to the IDE that you are using.
+
+## License
+
+This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
---
+++ src/main/java/net/crescent/crescentsultimatemod/CrescentSUltimateModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.crescent.crescentsultimatemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CrescentSUltimateModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
