1d5d61a2604e62eeac938796950d4299621fd594
Crescent <trtonoy2@gmail.com>
2026-09-26 02:32:18 +0600

    added new item

--- src/main/java/net/crescent/crescentsultimatemod/CrescentSUltimateModDataGenerator.java
+++ src/main/java/net/crescent/crescentsultimatemod/CrescentSUltimateModDataGenerator.java
@@ -1,5 +1,6 @@
 package net.crescent.crescentsultimatemod;
 
+import net.crescent.crescentsultimatemod.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -7,6 +8,7 @@ public class
 CrescentSUltimateModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+	var pack = fabricDataGenerator.createPack();
+	pack.addProvider(ModModelProvider::new);
 	}
 }
---
+++ src/main/java/net/crescent/crescentsultimatemod/datagen/ModModelProvider.java
@@ -0,0 +1,25 @@
+package net.crescent.crescentsultimatemod.datagen;
+
+import net.crescent.crescentsultimatemod.item.ModItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import org.jspecify.annotations.NonNull;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricPackOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(@NonNull BlockModelGenerators blockModelGenerators) {
+
+    }
+
+    @Override
+    public void generateItemModels(@NonNull ItemModelGenerators itemModelGenerators) {
+        itemModelGenerators.generateFlatItem(ModItems.URANIUM, ModelTemplates.FLAT_ITEM);
+    }
+}
